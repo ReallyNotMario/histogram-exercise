ae36395af04ecf7f04692ebf7bdfe5fdf4f81fa4
Mario Felipe Munoz <mariofelipemunozferrante@gmail.com>
2021-05-19 14:15:16 -0400

    Shortened datafiller.sh and added null address parsing. histogram.py now parses extracted data instead of dummy data.

--- datafiller.sh
+++ datafiller.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# first make the baseline file
-echo "first make the baseline file"
-curl -X GET "https://api.covalenthq.com/v1/pricing/tickers/?format=csv&key=ckey_fb5d7d485dc9431cb042bb93474" -H "Accept: application/json" > tempdata.csv
+# manually delete tempdata.csv and nulladdresses.csv if you want a clean slate
 while true; do
-sleep 10 
-echo "then delete the first line from the new csv file and append it to the old one"
-# then delete the first line from the new csv file and append it to the old one
+echo "Fetching data."
 curl -X GET "https://api.covalenthq.com/v1/pricing/tickers/?format=csv&key=ckey_fb5d7d485dc9431cb042bb93474" -H "Accept: application/json" | sed 1d >> tempdata.csv
+echo "Filtering null addresses"
+grep "0x0000000000000000000000000000000000000000" tempdata.csv >> nulladdresses.csv
+sleep 60
 done
--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
-datasource = Path("./", "data.xls")
-data = pd.read_excel(datasource)
-graph = data.plot(kind="bar", stacked=True, title="Number of Null Address Fields per Currency")
-graph.set_xticklabels(data['Crypto'], rotation = 0)
+datasource = Path("./", "tempdata.csv")
+data = pd.read_csv(datasource)
+graph = data.plot(kind="bar", title="Number of Null Address Fields per Currency")
+graph.set_xticklabels(data['contract_ticker_symbol'], rotation = 0)
 graph.get_figure().savefig('graph.pdf')
