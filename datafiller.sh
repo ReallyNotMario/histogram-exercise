9545f7fec482aa3eda9bc2da57762c7aaf45069b
Mario Felipe Munoz <mariofelipemunozferrante@gmail.com>
2021-05-19 14:39:50 -0400

    Added time limit to datafiller.sh

--- datafiller.sh
+++ datafiller.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# manually delete tempdata.csv and nulladdresses.csv if you want a clean slate
-while true; do
-echo "Fetching data."
-curl -X GET "https://api.covalenthq.com/v1/pricing/tickers/?format=csv&key=ckey_fb5d7d485dc9431cb042bb93474" -H "Accept: application/json" | sed 1d >> tempdata.csv
-echo "Filtering null addresses"
-grep "0x0000000000000000000000000000000000000000" tempdata.csv >> nulladdresses.csv
+# first make the baseline file
+echo "Making baseline file"
+curl -X GET "https://api.covalenthq.com/v1/pricing/tickers/?format=csv&key=ckey_fb5d7d485dc9431cb042bb93474" -H "Accept: application/json" > tempdata.csv
+grep "0x0000000000000000000000000000000000000000" tempdata.csv > nulladdresses.csv
+START=`date +%s`
+while [ $(( $(date +%s) - 3600 )) -lt $START ]; do
 sleep 60
+echo "Appending new csv file to the old one"
+# then delete the first line from the new csv file and append it to the old one
+curl -X GET "https://api.covalenthq.com/v1/pricing/tickers/?format=csv&key=ckey_fb5d7d485dc9431cb042bb93474" -H "Accept: application/json" | sed 1d >> tempdata.csv
+grep "0x0000000000000000000000000000000000000000" tempdata.csv >> nulladdresses.csv
 done
